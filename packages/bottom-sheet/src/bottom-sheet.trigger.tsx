d8fb753b2975f327a79b534de06a0a4d0fd096c4
Maina Miru <org.mainamiru@gmail.com>
2025-09-17 14:45:07 +0530

    initial

---
+++ packages/bottom-sheet/src/bottom-sheet.trigger.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const BottomSheetTrigger = () => {
+  return (
+    <View>
+      <Text>BottomSheetTrigger</Text>
+    </View>
+  );
+};
+
+export default BottomSheetTrigger;
---
+++ packages/bottom-sheet/src/bottom-sheet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const BottomSheet = () => {
+  return (
+    <View>
+      <Text>Bottom Sheet</Text>
+    </View>
+  );
+};
+
+export default BottomSheet;
